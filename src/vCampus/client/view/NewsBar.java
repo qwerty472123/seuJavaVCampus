7f817ef5e3c3370625f7658f6cc49f3495d541b0
klh <jskkk123@126.com>
2019-09-09 17:55:02 +0800

    modify News UI

--- src/vCampus/client/view/NewsBar.java
+++ src/vCampus/client/view/NewsBar.java
@@ -10,6 +10,11 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+
+import mdlaf.animation.MaterialUIMovement;
+import mdlaf.utils.MaterialColors;
+import vCampus.client.view.utility.MyStyle;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -24,7 +29,7 @@ public class NewsBar extends JPanel {
 	 */
 	public NewsBar() {
 		
-		setBackground(new Color(223, 223, 255));
+		setBackground(Color.white);
 		
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
@@ -34,43 +39,41 @@ public class NewsBar extends JPanel {
 		setLayout(gridBagLayout);
 		
 		submitButton = new JButton("刷新");
-		submitButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
+		submitButton.setForeground(MyStyle.LIGHT_GRAY);
+		submitButton.setBackground(Color.white);
+		submitButton.setFont(new Font("微软雅黑", Font.PLAIN, 30));
+		MaterialUIMovement.add(submitButton, MaterialColors.GRAY_100);
+		
 		GridBagConstraints gbc_1 = new GridBagConstraints();
 		gbc_1.insets = new Insets(0, 0, 0, 5);
 		gbc_1.gridx = 0;
 		gbc_1.gridy = 0;
 		gbc_1.gridwidth = 3;
 		add(submitButton, gbc_1);
-		submitButton.setFont(new Font("微软雅黑", Font.PLAIN, 20));
+		submitButton.setFont(new Font("微软雅黑", Font.PLAIN, 24));
 		submitButton.setHorizontalAlignment(SwingConstants.RIGHT);
 		
 		switchConstant = true;
 		foldupBtn = new JButton("→");
+		foldupBtn.setForeground(Color.black);
+		foldupBtn.setBackground(Color.white);
+		foldupBtn.setFont(new Font("微软雅黑", Font.PLAIN, 30));
+		MaterialUIMovement.add(foldupBtn, MaterialColors.GRAY_100);
+		
 		GridBagConstraints gbc_2 = new GridBagConstraints();
 		gbc_2.gridx = 4;
 		gbc_2.gridy = 0;
 		gbc_2.gridwidth = 3;
 		add(foldupBtn, gbc_2);
-		foldupBtn.setFont(new Font("微软雅黑", Font.PLAIN, 20));
-
-		/*
-		forwardBtn = new JButton("→");
-		GridBagConstraints gbc_3 = new GridBagConstraints();
-		gbc_3.gridx = 8;
-		gbc_3.gridy = 0;
-		gbc_3.gridwidth = 3;
-		add(forwardBtn, gbc_3);
-		forwardBtn.setFont(new Font("微软雅黑", Font.PLAIN, 20));
-		*/
+		foldupBtn.setFont(new Font("微软雅黑", Font.PLAIN, 24));
 
 	}
 	
 	public void switchNarrow() {
 		if (switchConstant) foldupBtn.setText("←");
 		else foldupBtn.setText("→");
+		switchConstant = !switchConstant;
+		foldupBtn.revalidate();
 	}
 	
 }
