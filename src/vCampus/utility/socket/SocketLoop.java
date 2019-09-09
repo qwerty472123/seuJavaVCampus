73f9491e02b6904e3fa4adfc6f3683bb1e6b0fb1
4qwerty7 <4qwerty7@163.com>
2019-09-09 11:12:50 +0800

    Reconnect feature

--- src/vCampus/utility/socket/SocketLoop.java
+++ src/vCampus/utility/socket/SocketLoop.java
@@ -6,7 +6,10 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.Deque;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 import vCampus.client.ClientMain;
 import vCampus.server.ServerMain;
@@ -17,31 +20,72 @@ import vCampus.utility.loop.Message;
 
 public class SocketLoop extends Loop {
 	
-	private Socket socket;
-	private ObjectInputStream in;
-	private ObjectOutputStream out;
+	private Socket socket = null;
+	private ObjectInputStream in = null;
+	private ObjectOutputStream out = null;
+	private boolean swap;
+	private UUID uuid;
+	private Object waitRecObj = new Object();
 
-	public SocketLoop(Map<String, Deque<LoopListener> > sharedListenerMap, Socket _socket, boolean swap) {
+	public SocketLoop(Map<String, Deque<LoopListener> > sharedListenerMap, Socket _socket, boolean _swap) {
 		super(sharedListenerMap);
 		socket = _socket;
+		swap = _swap;
+		initSocket();
+	}
+	
+	private boolean sendOldMsg(Deque<Message> msgs) {
+		Iterator<Message> iterator = msgs.iterator();
+        while (iterator.hasNext()) {
+        	Message msg = iterator.next();
+        	try {
+        		sendMsg(msg);
+        	} catch (Exception err) {
+        		Config.log(err);
+        		return false;
+        	}
+        	iterator.remove();
+        }
+        return true;
+	}
+	
+	@SuppressWarnings("unchecked")
+	private boolean initSocket() {
+		if (socket == null) return false;
 		try {
 			if (swap) {
 				in = new ObjectInputStream(socket.getInputStream());
 				out = new ObjectOutputStream(socket.getOutputStream());
+				uuid = (UUID) ClientMain.getTempData().get("uuid");
+				out.writeObject(uuid);
+				return sendOldMsg((Deque<Message>) ClientMain.getTempData().get("list"));
 			} else {
 				out = new ObjectOutputStream(socket.getOutputStream());
 				in = new ObjectInputStream(socket.getInputStream());
+				uuid = (UUID) in.readObject();
+				if (ServerMain.getLaterSenderMap().containsKey(uuid)) {
+					return sendOldMsg(ServerMain.getLaterSenderMap().get(uuid));
+				} else return true;
 			}
 		} catch (Exception e) {
 			in = null;
 			out = null;
 			Config.log(e);
+			return false;
 		}
 	}
 
 	@Override
 	protected Message getMsg() throws Exception {
-		if (in == null) return null;
+		if (in == null) {
+			if(swap) {
+				SocketReconnectHelper.triggerReconnect(false);
+				synchronized (waitRecObj) {
+					waitRecObj.wait();
+				}
+			}
+			else return null;
+		}
 		Message msg = null;
 		try {
 			msg = (Message)in.readObject();
@@ -49,6 +93,13 @@ public class SocketLoop extends Loop {
 			Config.log(e);
 			in = null;
 			out = null;
+			if (swap) {
+				SocketReconnectHelper.triggerReconnect(false);
+				synchronized (waitRecObj) {
+					waitRecObj.wait();
+				}
+				return getMsg();
+			}
 		}
 		return msg;
 	}
@@ -69,8 +120,26 @@ public class SocketLoop extends Loop {
 		}
 	}
 	
+	@SuppressWarnings("unchecked")
+	private void sendMsgLater(Message msg) {
+		if (Config.getType().equals("Client")) {
+			((Deque<Message>) ClientMain.getTempData().get("list")).add(msg);
+		} else if (Config.getType().equals("Server")) {
+			if (!ServerMain.getLaterSenderMap().containsKey(uuid)) {
+				ServerMain.getLaterSenderMap().put(uuid, new ConcurrentLinkedDeque<Message>());
+			}
+			ServerMain.getLaterSenderMap().get(uuid).add(msg);
+		} else {
+			Config.log("Error type for sendMsgLater!");
+		}
+	}
+	
 	public void sendMsg(Message msg) {
-		if (out == null) return;
+		if (out == null) {
+			sendMsgLater(msg);
+			if (swap) SocketReconnectHelper.triggerReconnect(true);
+			return;
+		}
 		try {
 			if (Config.getType().equals("Client") && ClientMain.getTempData().containsKey("token")) {
 				msg.getData().put("token", ClientMain.getTempData().get("token"));
@@ -82,6 +151,8 @@ public class SocketLoop extends Loop {
 			Config.log(e);
 			in = null;
 			out = null;
+			sendMsgLater(msg);
+			if (swap) SocketReconnectHelper.triggerReconnect(true);
 		}
 	}
 	
@@ -94,6 +165,20 @@ public class SocketLoop extends Loop {
 			Config.log("Error type for sendMsgWithCb!");
 		}
 		sendMsg(msg);
+	}
+	
+	public boolean setSocket(Socket _socket) {
+		if (!swap) {
+			Config.log("Illegal call to setSocket!");
+			return false;
+		}
+		socket = _socket;
+		if(initSocket()) {
+			synchronized(waitRecObj) {
+				waitRecObj.notifyAll();
+			}
+			return true;
+		}else return false;
 	}
 	
 }
